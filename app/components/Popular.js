eae0f11faeb3caf37d98b906a71254a787e8e153
tlabna <tlabna@users.noreply.github.com>
2017-05-26 11:13:07 -0400

    func: Remove language selection to it's own Stateless Functional Component
    1. Remove language selection from Popular component since this component will be split into two (language selection tab and grid for display for each language)
    2. Create SelectLanguage stateless functional component (SFC) and add selectLanguage logic here. Best to use this kind of function since this component is just a presentational component (i.e. only a render method) and it's best to separate from other components
    3. Add PropTypes to enforce checking on props and make sure it is required and of correct type
    4. Remove this keyword from language select since using SFC and React passes props as 1st argument

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,42 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+// This is a Stateless Functional Component (SFC).
+// If a component only has a render method
+// then it's best practice to use this kind of function.
+// Using SFCs is good for separating persentational components from other components.
+// It has no state and it's a function, hence, SFC
+// Note: React passes props to function as 1st arg
+function SelectLanguage (props) {
+	var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+	
+	return (
+		<ul className ='languages'>
+		{/* NOTE: When using the keyword function (as below), this creates a new context and *THIS* outside the functiion
+		is not the same as *THIS* inside the function. Therefore, to have the same *THIS* as the outer context, *THIS*
+		is added as a 2nd argument to .map */}
+			{languages.map(function (lang) {
+				return (
+					<li
+						style = {lang === props.selectedLanguage ? {color: '#D0021B'} : null}
+						// Using .bind on onClick event so that we can pass
+						// a specific language. First argument is null because 
+						// we have already bound *this* to it
+						// Function will only be invoked on click 
+						onClick = {props.onSelect.bind(null, lang)}  
+						key = {lang}>
+						{lang}
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
+
+SelectLanguage.propTypes = {
+	selectedLanguage: PropTypes.string.isRequired,
+	onSelect: PropTypes.func.isRequired
+};
 
 // Component definition
 class Popular extends React.Component {
@@ -21,27 +59,13 @@ class Popular extends React.Component {
 	}
 
 	render() {
-		var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 		return (
-			<ul className ='languages'>
-			{/* NOTE: When using the keyword function (as below), this creates a new context and *THIS* outside the functiion
-			is not the same as *THIS* inside the function. Therefore, to have the same *THIS* as the outer context, *THIS*
-			is added as a 2nd argument to .map */}
-				{languages.map(function (lang) {
-					return (
-						<li
-							style = {lang === this.state.selectedLanguage ? {color: '#D0021B'} : null}
-							// Using .bind on onClick event so that we can pass
-							// a specific language. First argument is null because 
-							// we have already bound *this* to it
-							// Function will only be invoked on click 
-							onClick = {this.updateLanguage.bind(null, lang)}  
-							key = {lang}>
-							{lang}
-						</li>
-					);
-				}, this)}
-			</ul>
+			<div>
+				<SelectLanguage 
+					selectedLanguage={this.state.selectedLanguage}
+					onSelect={this.updateLanguage}
+				/>
+			</div>
 		);
 	}
 }
