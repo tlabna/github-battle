e2144a6ed2ecb56ad0ba09ef65b74e1ab08108ae
tlabna <tlabna@users.noreply.github.com>
2017-05-29 11:16:37 -0400

    func: Add react-router for Nav links (Home, Battle, Popular)
    1. Add components for Home and Battle pages
    2. Add react-router
    3. Add CSS to nav links and Home page
    4. Update webpack.config.js to allow testing urls on dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,19 @@ module.exports = {
 	entry: './app/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'index_bundle.js'
+		filename: 'index_bundle.js',
+		// Sets base path for all assets
+		publicPath: '/'
 	},
 	module: {
 		rules: [
 			{ test: /\.(js)$/, use: 'babel-loader' },
 			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
 		]
+	},
+	// Allows us to request urls from browser
+	devServer: {
+		historyApiFallback: true
 	},
 	plugins: [new HtmlWebpackPlugin({
 		template: 'app/index.html'
