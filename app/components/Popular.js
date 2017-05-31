a276af3a559b5372321107288640f63f9fb57d62
tlabna <tlabna@users.noreply.github.com>
2017-05-31 12:26:30 -0400

    func: Add Loading component
    1. Create Loading component
    2. Add default props to Loading component
    3. Update Results and Popular components to use Loading component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 // This is a Stateless Functional Component (SFC).
 // If a component only has a render method
@@ -112,7 +113,7 @@ class Popular extends React.Component {
 					onSelect={this.updateLanguage}
 				/>
 				{!this.state.repos
-					? <p>LOADING</p>
+					? <Loading />
 					: <RepoGrid repos={this.state.repos} />
 				}
 			</div>
