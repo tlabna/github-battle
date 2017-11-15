f6c34eef380255f4c69c61db65d0075ed41b2414
tlabna <tlabna@users.noreply.github.com>
2017-11-15 18:12:05 -0500

    refactor: update code to use async/await

--- app/components/Results.js
+++ app/components/Results.js
@@ -58,27 +58,26 @@ class Results extends React.Component {
 		loading: true
 	}
 
-	componentDidMount () {
+	async componentDidMount () {
 		const {playerOneName, playerTwoName} = queryString.parse(this.props.location.search);
 		
-		battle([
-			playerOneName,
-			playerTwoName
-		]).then((results) => {
-			if (results === null) {
-				return this.setState(() => ({
-					error  : 'Looks like there was an error. Check that both users exists on Github',
-					loading: false
-				}));
-			}
+		const results = await battle([
+			playerOneName, playerTwoName
+		]);
 
+		if (results === null) {
 			this.setState(() => ({
-				error  : null,
-				winner : results[0],
-				loser  : results[1],
+				error  : 'Looks like there was an error. Check that both users exists on Github',
 				loading: false
 			}));
-		});
+		}	
+		
+		this.setState(() => ({
+			error  : null,
+			winner : results[0],
+			loser  : results[1],
+			loading: false
+		}));
 	}
 
 	render() {
