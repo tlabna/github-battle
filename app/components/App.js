635924682931e9406465c3d20e6a586be2422186
tlabna <tlabna@users.noreply.github.com>
2017-11-15 11:12:57 -0500

    refactor: Convert App.js & PlayerPreview.js to ES6

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,12 @@
-var React = require('react');
-var Popular = require('./Popular');
-var ReactRouter = require('react-router-dom');
-var Router = ReactRouter.BrowserRouter;
-var Route = ReactRouter.Route;
-var Switch = ReactRouter.Switch;
-var Nav = require('./Nav');
-var Home = require('./Home');
-var Battle = require('./Battle');
-var Results = require('./Results');
+const React = require('react');
+const Popular = require('./Popular');
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const {Route, Switch} = ReactRouter;
+const Nav = require('./Nav');
+const Home = require('./Home');
+const Battle = require('./Battle');
+const Results = require('./Results');
 
 
 // Component definition
@@ -22,9 +21,7 @@ class App extends React.Component {
 						<Route exact path='/battle' component={Battle} />
 						<Route path='/battle/results' component={Results} />
 						<Route path='/popular' component={Popular} />
-						<Route render={function () {
-							return <p>Not Found</p>;
-						}} />
+						<Route render={() => <p>Not Found</p>} />
 					</Switch>
 				</div>
 			</Router>
