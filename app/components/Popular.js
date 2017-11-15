6620f6e6574729f88138b778b25c0ccb8e9c291b
tlabna <tlabna@users.noreply.github.com>
2017-11-15 10:10:38 -0500

    refactor: Convert Popular.js to ES6

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,7 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var api = require('../utils/api');
-var Loading = require('./Loading');
+const React = require('react');
+const PropTypes = require('prop-types');
+const api = require('../utils/api');
+const Loading = require('./Loading');
 
 // This is a Stateless Functional Component (SFC).
 // If a component only has a render method
@@ -9,54 +9,50 @@ var Loading = require('./Loading');
 // Using SFCs is good for separating persentational components from other components.
 // It has no state and it's a function, hence, SFC
 // Note: React passes props to function as 1st arg
-function SelectLanguage (props) {
-	var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+function SelectLanguage ({selectedLanguage, onSelect}) {
+	const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 	
 	return (
 		<ul className ='languages'>
 		{/* NOTE: When using the keyword function (as below), this creates a new context and *THIS* outside the functiion
 		is not the same as *THIS* inside the function. Therefore, to have the same *THIS* as the outer context, *THIS*
 		is added as a 2nd argument to .map */}
-			{languages.map(function (lang) {
-				return (
-					<li
-						style = {lang === props.selectedLanguage ? {color: '#D0021B'} : null}
-						// Using .bind on onClick event so that we can pass
-						// a specific language. First argument is null because 
-						// we have already bound *this* to it
-						// Function will only be invoked on click 
-						onClick = {props.onSelect.bind(null, lang)}  
-						key = {lang}>
-						{lang}
-					</li>
-				);
-			})}
+			{languages.map((lang) => (
+				<li
+					style = {lang === selectedLanguage ? {color: '#D0021B'} : null}
+					// Using .bind on onClick event so that we can pass
+					// a specific language. First argument is null because 
+					// we have already bound *this* to it
+					// Function will only be invoked on click 
+					onClick = {onSelect(lang)}  
+					key = {lang}>
+					{lang}
+				</li>
+			))}
 		</ul>
 	);
 }
 
-function RepoGrid (props) {
+function RepoGrid ({ repos }) {
 	return (
 		<ul className='popular-list'>
-			{props.repos.map(function (repo, index) {
-				return (
-					<li key={repo.name} className='popular-item'>
-						<div className='popular-rank'>#{index + 1}</div>
-						<ul className='space-list-items'>
-							<li>
-								<img 
-									className='avatar'
-									src={repo.owner.avatar_url}
-									alt={'Avatar for ' + repo.owner.login}
-								/>
-							</li>
-							<li><a href={repo.html_url}>{repo.name}</a></li>
-							<li>@{repo.owner.login}</li>
-							<li>{repo.stargazers_count} stars</li>
-						</ul>
-					</li>
-				);
-			})}			
+			{repos.map(({name, owner, html_url, stargazers_count}, index) => (
+				<li key={name} className='popular-item'>
+					<div className='popular-rank'>#{index + 1}</div>
+					<ul className='space-list-items'>
+						<li>
+							<img 
+								className='avatar'
+								src={owner.avatar_url}
+								alt={'Avatar for ' + owner.login}
+							/>
+						</li>
+						<li><a href={html_url}>{name}</a></li>
+						<li>@{owner.login}</li>
+						<li>{stargazers_count} stars</li>
+					</ul>
+				</li>
+			))}			
 		</ul>
 	);
 }
@@ -88,33 +84,27 @@ class Popular extends React.Component {
 	}
 
 	updateLanguage (lang) {
-		this.setState(function () {
-			return {
-				selectedLanguage: lang,
-				repos: null
-			};
-		});
+		this.setState(() => ({
+			selectedLanguage: lang,
+			repos: null
+		}));
 
 		api.fetchPopularRepos(lang)
-			.then(function (repos) {
-				this.setState(function () {
-					return {
-						repos: repos
-					};
-				});
-			}.bind(this));
+			.then((repos) => this.setState(() => ({ repos })));
 	}
 
 	render() {
+		const {selectedLanguage, repos} = this.state;
+
 		return (
 			<div>
 				<SelectLanguage 
-					selectedLanguage={this.state.selectedLanguage}
+					selectedLanguage={selectedLanguage}
 					onSelect={this.updateLanguage}
 				/>
-				{!this.state.repos
+				{!repos
 					? <Loading />
-					: <RepoGrid repos={this.state.repos} />
+					: <RepoGrid repos={repos} />
 				}
 			</div>
 		);
